fixed some stupid potential field bugs now they work but now the force/torque strengths need to be reassed also torques need to always draw "between 2 circles" this is literally up to a sign you might need to pull of some hackery to get that

python_code/continuous/getForcesFromNeighbourhoods.py
@@ -1,13 +1,10 @@
 import numpy as np
 
-def calcTorqueRob(pos, robot_states, robRobNeig, v_hat, nOfRobots):
+
+def calcTorqueRob(pos, robRobNeig, v_hat, nOfRobots, particle_radius):
     torque_rob = np.zeros(nOfRobots)
 
     for i in range(nOfRobots):
-        # if robot state is != 0 then there is no effect
-        if robot_states[i] != 0:
-            torque_rob[i] = 0
-            continue
 
         nOfRobotsInNeigh = len(robRobNeig[i])
 
@@ -19,7 +16,9 @@ def calcTorqueRob(pos, robot_states, robRobNeig, v_hat, nOfRobots):
             rnorms_rob = np.linalg.norm(r_rob, axis=1).reshape((nOfRobotsInNeigh,1))
             r_rob_hat  = r_rob / rnorms_rob
             dots_rob = np.sum(v_hat[i] * r_rob_hat, axis=1).reshape((nOfRobotsInNeigh,1))
-            coefs = dots_rob / rnorms_rob**2 
+            coefs = dots_rob / (rnorms_rob - particle_radius)**2 
+            # NOTE maybe this should be without the square!
+            # TODO try both!
             crosses = np.cross(v_hat[i], r_rob_hat).reshape((nOfRobotsInNeigh, 1))
             particle_torques = coefs * crosses
             torque_rob[i] = np.sum(particle_torques) 
@@ -27,16 +26,36 @@ def calcTorqueRob(pos, robot_states, robRobNeig, v_hat, nOfRobots):
     return torque_rob
 
 
+def calcTorqueRob_as_v(v, pos, robRobNeig, v_hat, nOfRobots, particle_radius):
+    torque_rob = np.zeros((nOfRobots,2))
 
-def calcTorqueItem(pos, robot_states, robItemNeig, v_hat, nOfRobots, nOfItems):
+    for i in range(nOfRobots):
+
+        nOfRobotsInNeigh = len(robRobNeig[i])
+
+        if nOfRobotsInNeigh == 0:
+            torque_rob[i] = np.zeros(2)
+        else:
+            pos_neighbs = np.array([pos[n] for n in robRobNeig[i]]) if nOfRobotsInNeigh > 0 else 0
+            r_rob = pos[i] - pos_neighbs
+            rnorms_rob = np.linalg.norm(r_rob, axis=1).reshape((nOfRobotsInNeigh,1))
+            r_rob_hat  = r_rob / rnorms_rob
+            dots_rob = np.sum(v_hat[i] * r_rob_hat, axis=1).reshape((nOfRobotsInNeigh,1))
+            # NOTE maybe this should be without the square!
+            # TODO try both!
+            coefs = dots_rob / (rnorms_rob - particle_radius)**2 
+            crosses = np.cross(v_hat[i], r_rob_hat).reshape((nOfRobotsInNeigh, 1))
+            particle_torques = coefs * crosses
+            summ = np.sum(particle_torques) 
+            torque_rob[i] = v * np.array([np.cos(summ), np.sin(summ)])
+    return torque_rob
+
+
+def calcTorqueItem(pos, robItemNeig, v_hat, nOfRobots):
 
     torque_item = np.zeros(nOfRobots)
     # calculate torque for each particle based on nbhd
     for i in range(nOfRobots):
-        # if robot state is != 0 then there is no effect
-        if robot_states[i] != 0:
-            torque_item[i] = 0
-            continue
 
         nOfItemsInNeigh = len(robItemNeig[i])
 
@@ -52,6 +71,8 @@ def calcTorqueItem(pos, robot_states, robItemNeig, v_hat, nOfRobots, nOfItems):
             r_item_hat  = r_item / rnorms_item
             # dot 'em. dot does not support axis thing so we do it like this 
             dots_item = np.sum(v_hat[i] * r_item_hat, axis=1).reshape((nOfItemsInNeigh,1))
+            # NOTE maybe this should be without the square!
+            # TODO try both!
             coefs_item = dots_item / rnorms_item**2 
             # try repelling them now
             # crosses v_i with r_i and does so for all i
@@ -64,7 +85,8 @@ def calcTorqueItem(pos, robot_states, robItemNeig, v_hat, nOfRobots, nOfItems):
 
 
 
-def calcTorqueObs(pos, robot_states, robObsNeig, v_hat, nOfRobots, obstacleRadius):
+
+def calcTorqueObs(pos, robObsNeig, v_hat, nOfRobots, obstacleRadius):
 
     torque_obs = np.zeros(nOfRobots)
     # calculate torque for each particle based on nbhd
@@ -85,7 +107,9 @@ def calcTorqueObs(pos, robot_states, robObsNeig, v_hat, nOfRobots, obstacleRadiu
         r_obs_hat  = r_obs / rnorms_obs
         # dot 'em. dot does not support axis thing so we do it like this 
         dots_obs = np.sum(v_hat[i] * r_obs_hat, axis=1).reshape((nOfObssInNeigh,1))
-        coefs_obs = dots_obs / rnorms_obs**2 
+        # NOTE maybe this should be without the square!
+        # TODO try both!
+        coefs_obs = dots_obs / (rnorms_obs - obstacleRadius)**2 
         # try repelling them now
         # crosses v_i with r_i and does so for all i
         crosses_obs = np.cross(v_hat[i], r_obs_hat).reshape((nOfObssInNeigh, 1))
@@ -98,7 +122,45 @@ def calcTorqueObs(pos, robot_states, robObsNeig, v_hat, nOfRobots, obstacleRadiu
 
 
 
-def calcForceRob(v, pos, robot_states, robRobNeig, v_hat, nOfRobots, particle_radius):
+
+def calcTorqueObs_as_v(v, pos, robObsNeig, v_hat, nOfRobots, obstacleRadius):
+
+    torque_obs = np.zeros((nOfRobots,2))
+    # calculate torque for each particle based on nbhd
+    for i in range(nOfRobots):
+        # if robot state is != 0 then there is no effect
+        nOfObssInNeigh = len(robObsNeig[i])
+
+        if nOfObssInNeigh == 0:
+            torque_obs[i] = 0
+            torque_obs[i] = np.zeros(2)
+            continue
+        # calculate direction vector to things in neighbourhood
+        r_obs = pos[i] - np.array(list(map(list, robObsNeig[i])))
+        # calculate the norm of every direction vector
+        # the robot will never get inside the obstacle so minus it to have reasonable forces
+        rnorms_obs = np.linalg.norm(r_obs, axis=1).reshape((nOfObssInNeigh,1)) - obstacleRadius
+        # collect only nearby ones
+        # we need rhat
+        r_obs_hat  = r_obs / rnorms_obs
+        # dot 'em. dot does not support axis thing so we do it like this 
+        dots_obs = np.sum(v_hat[i] * r_obs_hat, axis=1).reshape((nOfObssInNeigh,1))
+        # NOTE maybe this should be without the square!
+        # TODO try both!
+        coefs_obs = dots_obs / (rnorms_obs - obstacleRadius)**2 
+        # try repelling them now
+        # crosses v_i with r_i and does so for all i
+        crosses_obs = np.cross(v_hat[i], r_obs_hat).reshape((nOfObssInNeigh, 1))
+        particle_torques_obs = coefs_obs * crosses_obs
+
+        summ = np.sum(particle_torques_obs) 
+        torque_obs[i] = v * np.array([np.cos(summ), np.sin(summ)]) 
+
+    return torque_obs
+
+
+
+def calcForceRob(v, pos, robRobNeig, nOfRobots, particle_radius):
 
     force_rob = np.zeros((nOfRobots,2))
     for i in range(nOfRobots):
@@ -113,7 +175,9 @@ def calcForceRob(v, pos, robot_states, robRobNeig, v_hat, nOfRobots, particle_ra
             r_rob_hat  = r_rob / rnorms_rob
             #rob_forces = np.array([r_rob_hat[p] / (rnorms_rob[p] - particle_radius)**2 for p in range(nOfRobotsInNeigh)])
             # trying it without the square 'cos it seemed way too small
-            rob_forces = np.array([r_rob_hat[p] / (rnorms_rob[p] - particle_radius) for p in range(nOfRobotsInNeigh)])
+        # NOTE maybe this should be without the square!
+        # TODO try both!
+            rob_forces = np.array([r_rob_hat[p] / (rnorms_rob[p] - particle_radius)**2 for p in range(nOfRobotsInNeigh)])
             force =  np.sum(rob_forces) 
             # it works because it's per element
             force_rob[i] = force if np.abs(force) < v else 0.05 * np.sign(force)
@@ -123,7 +187,7 @@ def calcForceRob(v, pos, robot_states, robRobNeig, v_hat, nOfRobots, particle_ra
 
 
 
-def calcForceItem(v, pos, robot_states, robItemNeig, v_hat, nOfRobots, nOfItems, particle_radius):
+def calcForceItem(v, pos, robItemNeig, nOfRobots, particle_radius):
 
     force_item = np.zeros((nOfRobots,2))
     for i in range(nOfRobots):
@@ -134,6 +198,8 @@ def calcForceItem(v, pos, robot_states, robItemNeig, v_hat, nOfRobots, nOfItems,
             r_item = pos[i] - np.array(list(map(list, robItemNeig[i])))
             rnorms_item = np.linalg.norm(r_item, axis=1).reshape((nOfItemsInNeigh,1))
             r_item_hat  = r_item / rnorms_item
+        # NOTE maybe this should be without the square!
+        # TODO try both!
             item_forces = np.array([r_item_hat[p] / (rnorms_item[p])**2 for p in range(nOfItemsInNeigh)])
             force =  np.sum(item_forces) 
             force_item[i] = force if np.abs(force) < v else v * np.sign(force)
@@ -143,7 +209,7 @@ def calcForceItem(v, pos, robot_states, robItemNeig, v_hat, nOfRobots, nOfItems,
 
 
 
-def calcForceObs(v, pos, robot_states, robObsNeig, v_hat, nOfRobots, nOfItems, particle_radius, obstacleRadius):
+def calcForceObs(v, pos, robObsNeig, nOfRobots, particle_radius, obstacleRadius):
 
     force_obs = np.zeros((nOfRobots,2))
     for i in range(nOfRobots):
@@ -154,6 +220,8 @@ def calcForceObs(v, pos, robot_states, robObsNeig, v_hat, nOfRobots, nOfItems, p
             r_obs = pos[i] - np.array(list(map(list, robObsNeig[i])))
             rnorms_obs = np.linalg.norm(r_obs, axis=1).reshape((nOfObssInNeigh,1))
             r_obs_hat  = r_obs / rnorms_obs
+            # NOTE maybe this should be without the square!
+            # TODO try both!
             obs_forces = np.array([r_obs_hat[p] / (rnorms_obs[p] - obstacleRadius)**2 for p in range(nOfObssInNeigh)])
             force =  np.sum(obs_forces) 
             force_obs[i] = force if np.abs(force) < v else v * np.sign(force)

python_code/continuous/runSim.py
@@ -11,7 +11,7 @@ from getVelocitiesFromStates import *
 # goes off in one direction for the amount of timesteps specified)
 def runSim(x, y, item_positions_set, delivery_station, N, nOfRobots, gridSize,  robot_statesPerTime, # sim params
                    v, particle_radius, torque_radius, obstacles,obstacleRadius,         # environment robot physical params 
-                   walkType, ni, trans_dif_T, rot_dif_T,                                          # random walk params
+                   walkType, ni, trans_dif_T, rot_dif_T, deviation,                                         # random walk params
                    T0, FR0, FI0, FO0,                                                   # artificial potential field parameters 
                    nOfUnstuckingSteps, stuckThresholdTime, stuckThresholdDistance       # unstucking parameters
                    ):
@@ -44,25 +44,13 @@ def runSim(x, y, item_positions_set, delivery_station, N, nOfRobots, gridSize,  
                          y[:, step].reshape((nOfRobots,1))))
         # also get the speeds for each particle
         # this already is vhat 'cos sin^2(x) + cos^2(x) = 1
-        v_hat = np.hstack((np.cos(fi) .reshape((nOfRobots,1)), 
-                            np.sin(fi).reshape((nOfRobots,1))))
+#        v_hat = np.hstack((np.cos(fi) .reshape((nOfRobots,1)), 
+#                            np.sin(fi).reshape((nOfRobots,1))))
 
         # get nbhds
         robRobNeig, robItemNeig, robObsNeig = getNeighbourhoods(pos, item_positions_list, 
                 nOfRobots, nOfItems, particle_radius, torque_radius, obstacles, obstacleRadius)
 
-        # the hw3 model is not good for this
-        force_rob = calcForceRob(v, pos, robot_states, robRobNeig, v_hat, nOfRobots, particle_radius)
-
-        #force_item = calcForceItem(v, pos, robot_states, robItemNeig, v_hat, nOfRobots, nOfItems, particle_radius)
-
-        force_obs = calcForceObs(v, pos, robot_states, robObsNeig,  v_hat, nOfRobots, nOfItems, particle_radius, obstacleRadius)
-
-        force_rob = FR0 * force_rob
-        #force_item = FI0 * force_item 
-        force_obs = FO0 * force_obs
-
-        torque_obs = calcTorqueObs(pos, robot_states, robObsNeig, v_hat, nOfRobots, obstacleRadius)
 
 
         ####################################################################################
@@ -80,7 +68,6 @@ def runSim(x, y, item_positions_set, delivery_station, N, nOfRobots, gridSize,  
         # do all calculations in separate functions
         explorers, returners, itemPickers = separateByState(robot_states, nOfRobots)
 
-        fi = fi - torque_obs
 
         # state 0 - active swimming (this is overwritten if it's some other state)
         # or handle state 4 - levy flight (going for some time in one direction)
@@ -182,9 +169,29 @@ def runSim(x, y, item_positions_set, delivery_station, N, nOfRobots, gridSize,  
             for robo in doneWithUnstucking:
                 unstuckers.pop(robo)
 
-            activeSwimmersStyleRW(fi, ni, v_hat, unstuckers)
+            swimmersBrownianStyle(fi, v_hat, unstuckers, deviation)
 
 
+
+        ####################################################################################
+        # superimpose forces
+        ####################################################################################
+
+        # the hw3 model is not good for this
+        force_rob = FR0 * calcForceRob(v, pos, robRobNeig, nOfRobots, particle_radius)
+        torque_rob = FR0 * calcTorqueRob_as_v(v, pos, robRobNeig, v_hat, nOfRobots, particle_radius)
+
+        force_item = FI0 * calcForceItem(v, pos, robItemNeig, nOfRobots, particle_radius)
+
+        force_obs = FO0 * calcForceObs(v, pos, robObsNeig,  nOfRobots, particle_radius, obstacleRadius)
+
+        # TODO put the coefficients in the force fuctions
+        # and make them < v! (otherwise they will "discontinuously" jump)
+
+        torque_obs = FO0 * calcTorqueObs_as_v(v, pos, robObsNeig, v_hat, nOfRobots, obstacleRadius)
+
+        # TODO TODO TODO CHECK WHETHER THIS MAKES SENSE
+        #fi = fi - torque_obs
 
         ####################################################################################
         # perform position updates 
@@ -193,13 +200,18 @@ def runSim(x, y, item_positions_set, delivery_station, N, nOfRobots, gridSize,  
 # TODO make should be chosen by some nice ifs because you will be tweaking it a lot
         x[:, step+1] = x[:,step] +  v * v_hat[:,0]
         x[:, step+1] = x[:, step+1] + force_rob[:,0] 
-        #x[:, step+1] = x[:, step+1] - force_item[:,0] 
-        #x[:, step+1] = x[:, step+1] + force_obs[:,0] 
+        x[:, step+1] = x[:, step+1] + torque_rob[:,0] 
+        x[:, step+1] = x[:, step+1] - force_item[:,0] 
+        x[:, step+1] = x[:, step+1] + force_obs[:,0] 
+        x[:, step+1] = x[:, step+1] + torque_obs[:,0] 
         x[:, step+1] = x[:, step+1] % gridSize
+
         y[:, step+1] = y[:,step] +  v * v_hat[:,1] 
         y[:, step+1] = y[:,step+1] + force_rob[:,1] 
-        #y[:, step+1] = y[:,step+1] - force_item[:,1] 
+        y[:, step+1] = y[:, step+1] + torque_rob[:,1] 
+        y[:, step+1] = y[:,step+1] - force_item[:,1] 
         y[:, step+1] = y[:,step+1] + force_obs[:,1] 
+        y[:, step+1] = y[:,step+1] + torque_obs[:,1] 
         y[:, step+1] = y[:,step+1] % gridSize
 
         